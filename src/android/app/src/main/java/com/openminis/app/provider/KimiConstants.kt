66a5b2ccfbe7cd73a73449390155652285a3c465
RikkaMinis Agent <rikkaminis@users.noreply.github.com>
2026-08-14 02:00:44 +0800

    chore: update KimiConstants KDoc (drop removed KimiDeviceFlow reference)

--- src/android/app/src/main/java/com/openminis/app/provider/KimiConstants.kt
+++ src/android/app/src/main/java/com/openminis/app/provider/KimiConstants.kt
@@ -2,7 +2,6 @@ package com.openminis.app.provider
 
 /**
  * Constants shared by Kimi Code provider components.
- * Formerly part of [KimiDeviceFlow] (OAuth device-flow class).
  */
 object KimiConstants {
     const val CODING_API_BASE = "https://api.kimi.com/coding"
